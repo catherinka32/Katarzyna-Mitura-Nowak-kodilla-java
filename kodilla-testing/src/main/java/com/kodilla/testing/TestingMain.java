29378fee555a1316c2c6d9e469197325c47b8b98
catherinka32 <kazia32@tlen.pl>
2017-07-13 14:22:25 +0200

    Second commit ufff

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing;
+
+/**
+ * Created by Mitura on 2017-07-13.
+ */
+public class TestingMain {
+    public static void main(String[] args){
+        System.out.println("Moduł 6. Wprowadznie do testowania oprogramowania");
+    }
+}
---
+++ src/main/java/com/kodilla/FirstClass.java
@@ -0,0 +1,7 @@
+package com.kodilla;
+
+/**
+ * Created by Mitura on 2017-06-29.
+ */
+public class FirstClass {
+}
