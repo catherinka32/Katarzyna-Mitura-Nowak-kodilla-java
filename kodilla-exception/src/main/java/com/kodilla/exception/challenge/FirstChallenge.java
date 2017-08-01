c87810fbfc4c4afb0dc91d3745bd0ede1b002286
catherinka32 <kazia32@tlen.pl>
2017-08-01 22:02:16 +0200

    Task 8.2

--- kodilla-exception/src/main/java/com/kodilla/exception/challenge/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/challenge/FirstChallenge.java
@@ -1,22 +1,22 @@
 package com.kodilla.exception.challenge;
 
 public class FirstChallenge {
-    public double divide(double a, double b) {
-        double result = 0;
-        try {
-            result = a/b;
-            System.out.println("Result is : " + result);
-        }catch (Exception e) {
-            System.out.println("Remember: don't divide by zero!!!");
-
-        }finally {
-            System.out.println("It's the end of action :)");
+    public double divide(double a, double b)throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
         }
-        return result;
+        return a / b;
     }
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
-        firstChallenge.divide(12.0, 0.0);
+        try{
+            double result = firstChallenge.divide(12.0, 0.0);
+            System.out.println("Result is: " + result);
+        }catch(ArithmeticException e ){
+            System.out.println("Remember: don't divide by zero!!!");
+        }finally{
+            System.out.println("It's the end of action :)");
+        }
     }
 }
