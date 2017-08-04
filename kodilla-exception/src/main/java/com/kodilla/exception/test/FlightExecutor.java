9d48d7a7a01dafdc929d6ed73f28493651b0e6e9
MagdaZawora <magda.zawora@gmail.com>
2017-08-04 18:46:14 +0200

    Task 8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightExecutor.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightExecutor.java
@@ -3,7 +3,11 @@ package com.kodilla.exception.test;
 public class FlightExecutor {
     public static void main(String args[]){
         FlightSearcher flightSearcher = new FlightSearcher();
-        Flight flight1 = new Flight("Oslo");
-        flightSearcher.findFlight(flight1);
+        Flight flight1 = new Flight("Bergamo");
+        try{
+            flightSearcher.findFlight(flight1);
+        }catch(Exception e){
+            System.out.println("Destination not found in the map");
+        }
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
@@ -5,21 +5,28 @@ import java.util.Map;
 
 public class FlightSearcher {
 
-    public void findFlight(Flight flight){
+    public void findFlight(Flight flight) throws RouteNotFoundException {
         HashMap<String, Boolean> flightMap = new HashMap<>();
 
-        flightMap.put("Newcastle",false);
-        flightMap.put("Milano",true);
-        flightMap.put("Malpensa",false);
-        flightMap.put("Oslo",true);
-        flightMap.put("Bergamo",true);
-
-        for(Map.Entry<String,Boolean>)entry : flightMap.entrySet()) {
-            if(entry.getKey()= flight.getDepartureAirport() && entry.getValue() = true){
-                System.out.println("Prepare money for a ticket :)");
-            }else{
-                System.out.println("Sorry, this destination is not available");
+        flightMap.put("Newcastle", false);
+        flightMap.put("Milano", true);
+        flightMap.put("Malpensa", false);
+        flightMap.put("Oslo", true);
+        flightMap.put("Bergamo", true);
+        boolean found = false;
+        for (Map.Entry<String, Boolean> entry : flightMap.entrySet()) {
+            if (entry.getKey() == flight.getDepartureAirport()) {
+                found = true;
+                if (entry.getValue()) {
+                    System.out.println("Prepare money for a ticket :)");
+                } else {
+                    System.out.println("Sorry, this destination is not available");
+                    }
+                }
             }
+        if (found == false) {
+            throw new RouteNotFoundException();
         }
     }
 }
+
