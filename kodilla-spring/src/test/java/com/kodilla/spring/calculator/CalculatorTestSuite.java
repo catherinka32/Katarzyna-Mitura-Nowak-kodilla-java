bb739e89b6d8f11acea2692a3fd052cace224a1f
catherinka32 <kazia32@tlen.pl>
2017-08-16 22:43:32 +0200

    Task 10.2

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,10 +17,14 @@ public class CalculatorTestSuite {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
         Calculator calculator= context.getBean(Calculator.class);
         //When
-        calculator.add(5,6);
-        calculator.sub(6,4);
-        calculator.mul(2,4);
-        calculator.div(6,3);
+        double add = calculator.add(5,6);
+        double sub = calculator.sub(6,4);
+        double mul = calculator.mul(2,4);
+        double div = calculator.div(6,3);
         //Then
+        Assert.assertEquals(11, add,0.0);
+        Assert.assertEquals(2, sub,0.0);
+        Assert.assertEquals(8, mul,0.0);
+        Assert.assertEquals(2, div,0.0);
     }
 }
