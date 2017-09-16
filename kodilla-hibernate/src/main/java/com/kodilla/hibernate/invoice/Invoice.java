0477c733d9cab2e27d100cbf593031e292c815f7
catherinka32 <kazia32@tlen.pl>
2017-09-16 21:43:12 +0200

    Copy

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Invoice.java
@@ -0,0 +1,51 @@
+package com.kodilla.hibernate.invoice;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name= "INVOICES")
+public class Invoice {
+    private int id;
+    private String number;
+    private List<Item> items = new ArrayList<>();
+
+    public Invoice(){
+
+    }
+
+    public Invoice(String number) {
+        this.number = number;
+
+    }
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "INVOICE_ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    @Column(name = "NUMBER")
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,targetEntity = Item.class,mappedBy = "invoice")
+    @Column(name = "ITEMS")
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Item;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    InvoiceDao invoiceDao;
+    @Autowired
+    ItemDao itemDao;
+    @Autowired
+    ProductDao productDao;
+    @Test
+    public void testInvoiceDaoSave(){
+        //Given
+
+        //When
+
+        //Then
+    }
+
+
+}
